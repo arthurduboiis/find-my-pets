8f2e289da3c71aed4c47e26d21ac23527fe50dda
Arthur DUBOIS <arthur.dbs70@gmail.com>
2024-10-09 14:37:22 +0200

    feat: add required dependencies

--- App.js
+++ App.js
@@ -1,12 +1,21 @@
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
+import styled from 'styled-components/native';
+const StyledContainer = styled.View`
+background-color: red; 
+flex: 1; 
+align-items: center;
+ justify-content: center;
+
+`;
+
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+    <StyledContainer>
+      <Text>Open up App.js to stop working on your app!</Text>
       <StatusBar style="auto" />
-    </View>
+    </StyledContainer>
   );
 }
 
