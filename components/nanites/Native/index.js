ab8dc6866413e406d5054596c1a03f9694e58fe9
jadegurnaud <jade.gurnaud@gmail.com>
2024-10-10 10:44:34 +0200

    init atom image

--- components/nanites/Native/index.js
+++ components/nanites/Native/index.js
@@ -1,5 +1,8 @@
 import StyledTitle from "./StyledTitle";
 import StyledSubTitle from "./StyledSubTitle";
 import StyledParagraph from "./StyledParagraph";
+import StyledImageBase from "./StyledImageBase";
+import StyledIconBase from "./StyledIconBase";
 
-export { StyledTitle, StyledSubTitle, StyledParagraph};
+
+export { StyledTitle, StyledSubTitle, StyledParagraph, StyledImageBase, StyledIconBase };
